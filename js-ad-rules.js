e3a80965f2eb8ab70b9e6465f5ce9f4b8bf073b5
MUSA <asdf131996455@gmail.com>
2025-11-30 20:56:55 +0800

    Create js-ad-rules.js

---
+++ js-ad-rules.js
@@ -0,0 +1,61 @@
+{
+    "*": {
+        "keywords": [
+            {
+                "selector": "li>a>font",
+                "keywords": [
+                    "澳门新葡京"
+                ]
+            }
+        ],
+        "classes": [
+            "ad",
+            "mda",
+            "notice",
+            "uni-swiper-slides"
+        ],
+        "selectors": [
+            "ads",
+            "div[style=\"23356\"]",
+            "div[style=\"opacity: 1;\"]",
+            "div[style=\"z-index: 2001;\"]",
+            "i.icon.iconfont.icon-account",
+            "iframe[style=\"position: absolute; display: block; left: 50%; top: 50%; width: 300px; height: 146px; margin: -73px 0px 0px -150px;\"]",
+            "uni-view.tabbar-list"
+        ],
+        "idsToHide": [
+            "__clb-spot_2090934_fxi_1",
+            "__clb-spot_2090934_mzi_1",
+            "rm-float3",
+            "啊路"
+        ],
+        "interval": 600
+    },
+    "_config": {
+        "defaultInterval": 600
+    },
+    "/cn\\d+\\.91short\\.com/": {
+        "keywords": [
+            {
+                "selector": "a>span.van-tab__text",
+                "keywords": [
+                    "一键脱衣",
+                    "催情春药",
+                    "同城约炮",
+                    "推荐"
+                ]
+            }
+        ],
+        "classes": [
+            "mda",
+            "notice",
+            "van-overlay",
+            "van-popup"
+        ],
+        "selectors": [
+            "span[style=\"color: gold\"]"
+        ],
+        "idsToHide": [],
+        "interval": 3000
+    }
+}
