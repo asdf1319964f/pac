d2d80086c27961873b78cd193b735b0d36b4bc29
MUSA <asdf131996455@gmail.com>
2025-11-30 21:13:55 +0800

    Update js-ad-rules.js

--- js-ad-rules.js
+++ js-ad-rules.js
@@ -1,61 +1,569 @@
 {
-    "*": {
+  "*": {
+    "keywords": [
+      {
+        "selector": "li>a>font",
         "keywords": [
-            {
-                "selector": "li>a>font",
-                "keywords": [
-                    "澳门新葡京"
-                ]
-            }
-        ],
-        "classes": [
-            "ad",
-            "mda",
-            "notice",
-            "uni-swiper-slides"
-        ],
-        "selectors": [
-            "ads",
-            "div[style=\"23356\"]",
-            "div[style=\"opacity: 1;\"]",
-            "div[style=\"z-index: 2001;\"]",
-            "i.icon.iconfont.icon-account",
-            "iframe[style=\"position: absolute; display: block; left: 50%; top: 50%; width: 300px; height: 146px; margin: -73px 0px 0px -150px;\"]",
-            "uni-view.tabbar-list"
-        ],
-        "idsToHide": [
-            "__clb-spot_2090934_fxi_1",
-            "__clb-spot_2090934_mzi_1",
-            "rm-float3",
-            "啊路"
-        ],
-        "interval": 600
-    },
-    "_config": {
-        "defaultInterval": 600
-    },
-    "/cn\\d+\\.91short\\.com/": {
+          "澳门新葡京"
+        ]
+      }
+    ],
+    "classes": [
+      "ad",
+      "mda",
+      "notice",
+      "uni-swiper-slides"
+    ],
+    "selectors": [
+      "ads",
+      "div[style=\"23356\"]",
+      "div[style=\"opacity: 1;\"]",
+      "div[style=\"z-index: 2001;\"]",
+      "i.icon.iconfont.icon-account",
+      "iframe[style=\"position: absolute; display: block; left: 50%; top: 50%; width: 300px; height: 146px; margin: -73px 0px 0px -150px;\"]",
+      "uni-view.tabbar-list"
+    ],
+    "idsToHide": [
+      "__clb-spot_2090934_fxi_1",
+      "__clb-spot_2090934_mzi_1",
+      "rm-float3",
+      "啊路"
+    ],
+    "interval": 600
+  },
+  "_config": {
+    "defaultInterval": 600
+  },
+  "/cn\\d+\\.91short\\.com/": {
+    "keywords": [
+      {
+        "selector": "a>span.van-tab__text",
         "keywords": [
-            {
-                "selector": "a>span.van-tab__text",
-                "keywords": [
-                    "一键脱衣",
-                    "催情春药",
-                    "同城约炮",
-                    "推荐"
-                ]
-            }
-        ],
-        "classes": [
-            "mda",
-            "notice",
-            "van-overlay",
-            "van-popup"
-        ],
-        "selectors": [
-            "span[style=\"color: gold\"]"
-        ],
-        "idsToHide": [],
-        "interval": 3000
-    }
+          "一键脱衣",
+          "催情春药",
+          "同城约炮",
+          "推荐"
+        ]
+      }
+    ],
+    "classes": [
+      "mda",
+      "notice",
+      "van-overlay",
+      "van-popup"
+    ],
+    "selectors": [
+      "span[style=\"color: gold\"]"
+    ],
+    "idsToHide": [],
+    "interval": 3000
+  },
+  "/xhs.+\\.(vip|cc)|ws.xhs.+\\.(vip|cc)/": {
+    "selectors": [
+      "div[class*=\"container\"] > [class*=\"van-overlay\"]",
+      "div[class*=\"cvfooter-\"]",
+      "#cvtop-hnrrwiswdbtynwcp",
+      ".notice-marquee",
+      ".header-container-top.header-container > .container",
+      ".s-video-b-btn-box.vod-share",
+      ".copy-domain-btn.vod-share",
+      ".swiper-backface-hidden.swiper-watch-progress.swiper-android.swiper-horizontal.swiper-initialized.swiper-fade.external-carousel",
+      ".fotxt",
+      ".footer-box",
+      ".love > .lozad-image",
+      "div.van-swipe-item > .lozad-image",
+      "[class*=\"left-\"][class*=\"-wrap\"].back-to-top2.js-show-ios",
+      "p:nth-of-type(7)",
+      "div.van-tab:nth-of-type(5)",
+      "div.van-tab:nth-of-type(7)",
+      "div.van-tab:nth-of-type(10)",
+      "div.van-tab:nth-of-type(11)",
+      ".abk-img",
+      ".s-link-s2",
+      ".s-footer-tel",
+      ".sc-text.tcjy",
+      ".sc-text.cysc",
+      "li.navbar-item:nth-of-type(10)",
+      "li.navbar-item:nth-of-type(11)",
+      ".twoColumnAdv",
+      ".textblock.vod-class.justify-center.flex",
+      "div.button > .sc-text",
+      "span.sc-text.rightBtn > div.button > span",
+      ".live_list",
+      ".light-block.container > .block-header",
+      "div.guaContainer-guaList-item:nth-of-type(6)",
+      "[class^=\"swiper-wrapper-\"]",
+      "[class*=\" swiper-wrapper-\"]",
+      "strong > a[href*=\"fqvm5e.top\"]",
+      "span.vodContainer-list-item:nth-of-type(1)",
+      "span.vodContainer-list-item:nth-of-type(6)",
+      "div.vodContainer-list-item:nth-of-type(6)",
+      "#abk-swiper1",
+      "#abk-swiper2",
+      ".tags",
+      ".app-abk.external-app",
+      "div.newPopup_container",
+      "div.newPopup:nth-of-type(6)",
+      "div.newPopup:nth-of-type(5)",
+      "div.newPopup:nth-of-type(3)",
+      "div.newPopup:nth-of-type(2)",
+      "div.newPopup:nth-of-type(4)",
+      "div.newPopup:nth-of-type(7)",
+      "div.newPopup:nth-of-type(8)",
+      "div.newPopup:nth-of-type(9)",
+      "div.liveBroadcast_adv",
+      "div.container.footer_container"
+    ]
+  },
+  "/mmzx\\d+.cc|cllzx\\d+.cc/": {
+    "selectors": [
+      ".containerad",
+      "ul.clearfix.tag-btn.tag:nth-of-type(1)",
+      ".footer",
+      "div > a[href^=\"https://ktdb\"]",
+      ".nav:nth-of-type(2)"
+    ]
+  },
+  "www.hjw01.com": {
+    "selectors": [
+      ".xqbj-list-rows-placard",
+      ".xqbj-component-advertises",
+      ".van-popup",
+      ".banner",
+      ".van-grid",
+      ".tags-group",
+      "#POPUP-WINDOW-GG",
+      "div.xqbj-list-rows:nth-of-type(1)",
+      "div.xqbj-list-rows:nth-of-type(2)"
+    ]
+  },
+  "ball.vlovslh.com": {
+    "selectors": [
+      ".xqbj-list-rows-placard",
+      ".xqbj-component-advertises",
+      ".van-popup",
+      ".banner",
+      ".van-grid",
+      ".tags-group",
+      "#POPUP-WINDOW-GG",
+      "div.xqbj-list-rows:nth-of-type(1)",
+      "div.xqbj-list-rows:nth-of-type(2)"
+    ]
+  },
+  "/haij\\.me/": {
+    "selectors": [
+      ".xqbj-list-rows-placard",
+      ".xqbj-component-advertises",
+      ".van-popup",
+      ".banner",
+      ".van-grid",
+      ".tags-group",
+      "#POPUP-WINDOW-GG",
+      "div.xqbj-list-rows:nth-of-type(1)",
+      "div.xqbj-list-rows:nth-of-type(2)"
+    ]
+  },
+  "91jav.fun": {
+    "selectors": [
+      ".being.bpoalxgf.xyz",
+      ".fine-wrap",
+      "div.col-lg-3.col-sm-4.col-6:nth-of-type(1)",
+      "div.col-lg-3.col-sm-4.col-6:nth-of-type(9)",
+      "div.col-lg-3.col-sm-4.col-6:nth-of-type(17)",
+      "div.col-xl-3.col-sm-4.col-6:nth-of-type(1)",
+      "div.col-xl-3.col-sm-4.col-6:nth-of-type(9)",
+      "div.col-lg-12.col-sm-4.col-6:nth-of-type(1)",
+      "div.col-lg-12.col-sm-4.col-6:nth-of-type(9)",
+      "div.col-lg-12.col-sm-4.col-6:nth-of-type(17)",
+      ".img-box > [href^=\"https://mdsfport.japaneast.cloudapp.azure.com:455/\"]"
+    ]
+  },
+  "/40425\\d+\\.(icu|top)/": {
+    "selectors": [
+      ".bannerimgs",
+      ".mpOgV",
+      "#container > span"
+    ]
+  },
+  "javdb.com": {
+    "selectors": [
+      ".app-desktop-banner > .container"
+    ]
+  },
+  "www.mxshm.site": {
+    "selectors": [
+      "#cZ116934920959256"
+    ]
+  },
+  "hjd2048.com": {
+    "selectors": [
+      ".nav-container",
+      ".promo-container",
+      "tr.mobile_index_1.f_one.tr3:nth-of-type(2)"
+    ]
+  },
+  "jable.one": {
+    "selectors": [
+      ".u8f83992ea7ab222e6158e9e0988efe30"
+    ]
+  },
+  "chinaautoparts.net": {
+    "selectors": [
+      ".container > .da",
+      ".video-type-box > .da",
+      "[href=\"/xinghuo.apk\"]",
+      "[id^=\"swipercontainer\"]"
+    ]
+  },
+  "llx6.buzz": {
+    "selectors": [
+      ".my_top_app"
+    ]
+  },
+  "www.longfeng45.top": {
+    "selectors": [
+      "div.aui-palace-three.aui-php:nth-of-type(3)",
+      "div.aui-palace-three.aui-php:nth-of-type(4)",
+      "div.ts-wrap:nth-of-type(3)",
+      ".Utop > .cate-section"
+    ]
+  },
+  "8.31xx7901a.cc": {
+    "selectors": [
+      "div:nth-of-type(7)",
+      "div.bcolor.menulist:nth-of-type(6)",
+      "div.menulist:nth-of-type(5)",
+      ".mx-auto.md\\:max-w-screen-xl"
+    ]
+  },
+  "rb.huaduys.org": {
+    "selectors": [
+      ".play_boxbg > div.container > .ad",
+      "#r3f4h9c51n",
+      "#play_page > div.container > .ad > li"
+    ]
+  },
+  "www.cllzx9.cc": {
+    "selectors": [
+      "div:nth-of-type(4)"
+    ]
+  },
+  "txcy-7oo1.txcy-pu.buzz": {
+    "selectors": [
+      "li.mb15.col-m-4.col-4 > [href^=\"http://38.49.9.4:11166/a.html\"]",
+      "div.mb15.col-m-24.col-12:nth-of-type(2)",
+      ".bdpic",
+      "div:nth-of-type(4)"
+    ]
+  },
+  "www.xgtoon.com": {
+    "selectors": [
+      "div.nuynrtHB"
+    ]
+  },
+  "krx18.com": {
+    "selectors": [
+      "#image-container",
+      ".right-position.chaty-widget",
+      "#__clb-spot_2033364_mdj_5_container"
+    ]
+  },
+  "44yu.cc": {
+    "selectors": [
+      "#\\38 257479454139528945-10491"
+    ]
+  },
+  "www.pornhk.org": {
+    "selectors": [
+      ".p-1.row",
+      ".nav-link",
+      "#top-ads",
+      "#fullPageModal",
+      "div.m-0.no-gutters.row:nth-of-type(1)",
+      "div.m-0.no-gutters.row:nth-of-type(4)",
+      "div.m-0.no-gutters.row:nth-of-type(5)",
+      "div.links.row:nth-of-type(9)",
+      ".m-0.no-gutters.row",
+      ".justify-content-between.d-sm-block.d-flex",
+      ".no-gutters.row.my-1.links"
+    ]
+  },
+  "goodav17.com": {
+    "selectors": [
+      "div.ads_all:nth-of-type(7)"
+    ]
+  },
+  "17av.one": {
+    "selectors": [
+      ".v3417f764877c098ea4dfa05746979f49",
+      "[href^=\"https://lvsa4ehx\"]",
+      ".player_bottom.cl",
+      ".t14945c43368a746b733d9589269aec72",
+      ".ea67752b749312f94a82e110889d67a91"
+    ]
+  },
+  "haoseqi.com": {
+    "selectors": [
+      "a.link > .img",
+      "#mobile_banner"
+    ]
+  },
+  "jmd8.com": {
+    "selectors": [
+      ".hkzqaofx_b"
+    ]
+  },
+  "pinkclips.mobi": {
+    "selectors": [
+      "[href=\"/gay\"]"
+    ]
+  },
+  "www.ffaaff.com": {
+    "selectors": [
+      "#favImg",
+      "#azwangye1",
+      ".favorites.modal",
+      "dl:nth-of-type(6)",
+      "dl:nth-of-type(7)",
+      "dl:nth-of-type(8)",
+      "dl:nth-of-type(9)",
+      "dl:nth-of-type(10)",
+      "dl:nth-of-type(11)"
+    ]
+  },
+  "www.htkt122.vip": {
+    "selectors": [
+      ".header-container > .container",
+      "div.container:nth-of-type(2)",
+      "[id^=\"footer-\"]",
+      ".top-5420dllhv04-wrap"
+    ]
+  },
+  "cn1.51-cg.com": {
+    "selectors": [
+      ".adm",
+      ".highlight-box",
+      "div.player-info:nth-of-type(4)"
+    ]
+  },
+  "ihakgsb.info": {
+    "selectors": [
+      ".top-title-content",
+      "div.showBox",
+      "section.notice",
+      "section:nth-of-type(1)"
+    ]
+  },
+  "18comic.vip": {
+    "selectors": [
+      ".c835e-33_e_sticky2"
+    ]
+  },
+  "91porn32.xyz": {
+    "selectors": [
+      ".j0c57cf55ca6a45c62d4b00dc10ed8a10",
+      ".qd4f96cae34e908286a9a3782617b3146"
+    ]
+  },
+  "bho3q1p7u6.shop": {
+    "selectors": [
+      "#floating-box"
+    ]
+  },
+  "d1xwta4tq724e9.cloudfront.net": {
+    "selectors": [
+      ".apps",
+      ".app-loading",
+      ".start_ad",
+      ".van-overlay > section",
+      ".van-overlay",
+      ".a"
+    ]
+  },
+  "asmrs.live": {
+    "selectors": [
+      ".tongji"
+    ]
+  },
+  "123.91jq285.work": {
+    "selectors": [
+      ".nsqah5tegwi.list-text-ad-links",
+      ".a8vz9k0olct.list-text-ad-links"
+    ]
+  },
+  "/.+jfg.+\\.com/": {
+    "selectors": [
+      ".appbox"
+    ]
+  },
+  "/htkt.+\\.vip/": {
+    "selectors": [
+      "div:nth-of-type(14) > div:nth-of-type(2) > div:nth-of-type(1)",
+      "top-drdb2pvxq34-wrap",
+      "top-gzw2rjmqs3f-wrap",
+      "top-sr12ujhjbl-wrap",
+      "div:nth-of-type(14) > div:nth-of-type(2) > div:nth-of-type(1) > div",
+      "div:nth-of-type(14) > div:nth-of-type(2) > div:nth-of-type(1) > a",
+      ".container > div:nth-of-type(3)",
+      ".header-container > .container",
+      ".top-hfl1dogokj-wrap",
+      ".top-e9tsruqm2fs-wrap",
+      ".top-lixvdfgor1k-wrap",
+      "#top-ii4ankqlcgm",
+      "#top-y523umo1x4",
+      "#top-snk6nglvfgg",
+      "#top-bl4rkp24h7m",
+      "#top-eb11kp8jxxa",
+      "#top-untv5ywaj5",
+      "#top-b7chzlc7i3"
+    ]
+  },
+  "www.kaiche119.cc": {
+    "selectors": [
+      "#app > .app > .navi"
+    ]
+  },
+  "f15.bzraizy.cc": {
+    "selectors": [
+      ".fine-wrap",
+      ".pb-3.video-info > .text-center > div"
+    ]
+  },
+  "d3qaaxft76zym9.cloudfront.net": {
+    "selectors": [
+      ".bottomBox",
+      ".headerBox"
+    ]
+  },
+  "1dpimsdt.7luanlun79.xyz": {
+    "selectors": [
+      ".main-container"
+    ]
+  },
+  "bingfong.com": {
+    "selectors": [
+      "p:nth-of-type(2)",
+      "div:nth-of-type(3)"
+    ]
+  },
+  "/(xzweimi\\d*|mvzsp|mvxzsp)\\.(cc|com)/": {
+    "selectors": [
+      ".lg\\:block.relative.w-full > .w-full.h-auto",
+      "div.van-overlay:nth-of-type(6)",
+      "div.cursor-pointer.overflow-hidden.w-full > .fill.bg-sub-color.fetch-image",
+      "div.dialog-wrap.w-full",
+      "div.van-overlay:nth-of-type(4)"
+    ]
+  },
+  "d2x03a61ogs2x5.cloudfront.net": {
+    "selectors": [
+      ".banner",
+      ".discount",
+      "#van-tab-3 > .van-tab__panel > .typeList > .van-pull-refresh > .van-pull-refresh__track > .van-list > .list > div.ad",
+      ".fixed",
+      ".apps",
+      ".van-overlay",
+      "#van-tab-2 > .van-tab__panel > .ads",
+      "#van-tab-3 > .van-tab__panel > .ads",
+      "#van-tab-4 > .van-tab__panel > .ads",
+      "#van-tab-5 > .van-tab__panel > .ads",
+      "#van-tab-6 > .van-tab__panel > .ads",
+      "#van-tab-7 > .van-tab__panel > .ads",
+      "#van-tab-8 > .van-tab__panel > .ads",
+      "#van-tab-9 > .van-tab__panel > .ads"
+    ]
+  },
+  "mh0619.djfrhyhtuo.work": {
+    "selectors": [
+      ".no.homepage.page"
+    ]
+  },
+  "mrdscgw.nl": {
+    "selectors": [
+      "div.my-4.\\34 8d458:nth-of-type(2)",
+      ".cf2e99 > .lg\\:grid-cols-2.grid-cols-1.gap-1.grid.my-4.\\34 8d458",
+      "div.basis-full.snap-center.flex-none.flex > .overflow-hidden.w-full > .lg\\:hidden.block.relative > .left-0.top-0.absolute.h-full.w-full > .cover.bg-theme-color.lg\\:bg-white.fetch-image"
+    ]
+  },
+  "xn---jfgdizhi-at-gmail-com-pe92bk82e4g1pmbse.jfgwww.com": {
+    "selectors": [
+      "[href^=\"https://vipalijow33dfdf.xpj75638.xyz\"]",
+      "[href=\"https://5463333bb.vip\"]"
+    ]
+  },
+  "www.2kcdsk.sbs": {
+    "selectors": [
+      "div.wrap:nth-of-type(3)",
+      "#subMenuBox > dl"
+    ]
+  },
+  "camp-lamp-plan.sexav-102.com": {
+    "selectors": [
+      "div.text-center.custom_link-wrapper:nth-of-type(3)",
+      ".tx-wide > div > div.text-center.custom_link-wrapper > .grid-container.custom_link",
+      ".row-m-space15.row > .text-center.custom_link-wrapper > .grid-container.custom_link"
+    ]
+  },
+  "d1x3rzq2txjc6c.cloudfront.net": {
+    "selectors": [
+      "#moveDiv",
+      ".splash_adv",
+      ".content",
+      ".announcementPopup",
+      ".persalePopup",
+      ".nineGridPopup",
+      "div[id=\"moveDiv\"]",
+      ".square:has(.squareImg)",
+      ".adv_swiper",
+      ".adv-container",
+      ".adv-swiper-slide",
+      ".homeHeader[data-type]:has(.rectAdvBox)",
+      ".rectAdvBox:has(.rectAdvIcon)",
+      ".adv-line-2",
+      ".fiveGrid:has(.rectAdvBox)",
+      ".headerAdv",
+      ".appList:has(.appBox)",
+      ".wrapper:has(.appList)",
+      ".imgWrapper:has(img[src*=\"cover\"])"
+    ]
+  },
+  "www.diaougj.com": {
+    "selectors": [
+      ".index-items-wrap",
+      ".p-card-box",
+      ".s-main-box-middle",
+      ".item-title-box",
+      "div.vs_dc3b5d1a3f59ee19cb5e6a4dbe162e0f-item-vide-n > .js-17c",
+      "div.s-x-img-wrap:nth-of-type(10)",
+      "[href^=\"/yp/0.html\"]",
+      "a.js-17c:nth-of-type(2)",
+      "a.js-17c:nth-of-type(8)"
+    ]
+  },
+  "ylib.avso73.icu": {
+    "selectors": [
+      "#link-2",
+      ".bl.ky",
+      ".esb.ky"
+    ]
+  },
+  "www.wt8r3r.sbs": {
+    "selectors": [
+      ".swiper-slide-active.swiper-slide > [href=\"javascript:void(0);\"]",
+      ".swiper-container-android.swiper-container-fade.swiper-container-horizontal.swiper-container",
+      "#subMenuBox > dl"
+    ]
+  },
+  "w2.xhsc1f6q.cc": {
+    "selectors": [
+      "div.newPopup:nth-of-type(13)",
+      "div.newPopup:nth-of-type(12)",
+      "div.newPopup:nth-of-type(10)"
+    ]
+  },
+  "www.pornfind.cc": {
+    "selectors": [
+      "#fullPageModal"
+    ]
+  }
 }
